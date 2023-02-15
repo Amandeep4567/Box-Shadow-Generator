2e29d4e3967d0b2737944f751245fa7a5055f717
Amandeep Kalakar <amankumaradk@gmail.com>
2023-02-15 22:04:25 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,19 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [Hori, setHori] = useState(10);
+  const [Veri, setVeri] = useState(10);
+  const [Blur, setBlur] = useState(10);
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="controls">
+        <label>Horizontal length</label>
+        <input type="range" min="-200" max="200" />
+      </div>
+      <div className="output"></div>
+    </div>
+  );
 }
 
 export default App;
